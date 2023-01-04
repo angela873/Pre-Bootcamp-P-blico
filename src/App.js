16365baa7bc40524ac8f53b65494673be04a65b1
angela873 <rocioangelagonzalez@gmail.com>
2023-01-03 21:18:46 -0300

    Poniendo todo junto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Lista from './componentes/Lista';
+function App() {
+  return (
+   <div>
+    <h1>
+      Hello Dojo
+    </h1>
+    <Lista>
+
+    </Lista>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Lista.js
@@ -0,0 +1,24 @@
+const Lista =()=>{
+    return(
+        <div>
+            <h2>
+                Cosas que necesito hacer
+            </h2>
+            <ul>
+                <li>
+                    Aprender React
+                </li>
+                <li>
+                    Subir monte Everest
+                </li>
+                <li>
+                    Correr una Maraton
+                </li>
+                <li>
+                    Alimentar a los perrros
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default Lista;
